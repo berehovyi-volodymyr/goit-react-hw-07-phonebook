41a3b92a80a214728533830efaf2c8bc28855eea
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2023-03-05 22:15:01 +0200

    +

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import { contactsApi } from './contacts/contacts-slice';
 import filterReducer from './filter/filter-slice';
 
