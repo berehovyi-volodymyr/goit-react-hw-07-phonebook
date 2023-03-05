545e80af82f6f5f3103a0f5670a4a237f59881f9
berehovyi-volodymyr <106471456+berehovyi-volodymyr@users.noreply.github.com>
2023-03-05 22:09:24 +0200

    delete comments

--- src/redux/contacts/contacts-selectots.js
+++ src/redux/contacts/contacts-selectots.js
@@ -1,12 +1,1 @@
 export const getAllContacts = store => store.contacts;
-
-// export const visibleContacts = ({ filter }) => {
-//   if (!getFilter) {
-//     return getAllContacts;
-//   }
-//   const normolizeFilter = filter.toLowerCase();
-
-//   return data.filter(contact =>
-//     contact.name.toLocaleLowerCase().includes(normolizeFilter)
-//   );
-// };
